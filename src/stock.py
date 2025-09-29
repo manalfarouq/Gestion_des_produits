75656e8c41a60970103e77770babdd4ebf2c2870
FAROUQI <farouqimanal@gmail.com>
2025-09-29 15:39:56 +0100

    upgrade - adding delete function

--- src/stock.py
+++ src/stock.py
@@ -18,17 +18,11 @@ def ajout_produit(product_name, product_quantite, product_prix):
         except ValueError:
             print("Erreur : La quantité doit être un entier et le prix doit être un nombre.")
 
-# def supression_produit(product_name):
-#     if product_name in df['nom_produit'].values:
-#         df.drop(df[df['nom_produit'] == product_name], inplace=True)
-#         df.to_csv('Products.csv', index=False)
-#         print("Produit supprimé avec succès !")
-#     else :
-#         print("Le produit n'existe pas.")
 
-# ajout_produit('Laptop', 10, 800.)
-# supression_produit('mouse')
-
-
-# print(df)
-print(df)
+def supression_produit(product_name):
+    if product_name in df['nom_produit'].values:
+        df.drop(df[df['nom_produit'] == product_name].index,inplace=True)
+        df.to_csv('Products.csv', index=False)
+        print("Produit supprimé avec succès !")
+    else :
+        print("Le produit n'existe pas.")
