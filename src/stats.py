e84e67984ef30841dc9c4d0f6e07326d4711ecf9
FAROUQI <farouqimanal@gmail.com>
2025-09-29 15:52:59 +0100

    statistics(plus cher et moins cher

--- src/stats.py
+++ src/stats.py
@@ -1,1 +1,15 @@
 #  stats file
+
+import pandas as pd
+
+df = pd.read_csv('Products.csv')
+
+def afficher_statistiques(dataframe_name):
+    total_produits = len(dataframe_name)
+    produit_plus_cher = dataframe_name.loc[df['prix_unitaire'].idxmax()]
+    produit_moins_cher = dataframe_name.loc[df['prix_unitaire'].idxmin()]
+
+    print(f"Nombre total de produits : {total_produits}")
+    print(f"Produit le plus cher : {produit_plus_cher['nom_produit']} à {produit_plus_cher['prix_unitaire']} DH")
+    print(f"Produit le moins cher : {produit_moins_cher['nom_produit']} à {produit_moins_cher['prix_unitaire']} DH")
+    
