359ca973abca251e94e85f49d9b64c47845d51c6
FAROUQI <farouqimanal@gmail.com>
2025-09-29 21:56:00 +0100

    visualisation avec des graphes en barres

--- src/stats.py
+++ src/stats.py
@@ -1,19 +1,1 @@
 #  stats file
-
-import pandas as pd
-
-df = pd.read_csv('Products.csv')
-
-def afficher_statistiques(dataframe_name):
-    total_produits = len(dataframe_name)
-    produit_plus_cher = dataframe_name.loc[df['prix_unitaire'].idxmax()]
-    produit_moins_cher = dataframe_name.loc[df['prix_unitaire'].idxmin()]
-
-    print(f"Nombre total de produits : {total_produits}")
-    print(f"Produit le plus cher : {produit_plus_cher['nom_produit']} à {produit_plus_cher['prix_unitaire']} DH")
-    print(f"Produit le moins cher : {produit_moins_cher['nom_produit']} à {produit_moins_cher['prix_unitaire']} DH")
-    
-def calcul_moyenne_prix(dataframe_name):
-    moyenne_prix = dataframe_name['prix_unitaire'].mean()
-    print(f"Prix moyen des produits : {moyenne_prix:.2f} DH")
-    
