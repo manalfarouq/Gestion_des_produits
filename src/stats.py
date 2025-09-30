dfe029fca2c6197b5f14e98f2f1fa9e9593e74ab
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-30 13:08:38 +0100

    update stats

--- src/stats.py
+++ src/stats.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("data/Products.csv")
+
+
 def get_min_price(df):
     return df["prix_unitaire"].min()
 
@@ -10,15 +15,16 @@ def get_avg_price(df):
     return df["prix_unitaire"].mean()
 
 
-def get_total_stock(df):
-    return df["quantite"].sum()
+def sum_multi_price_by_quantity(df):
+    results = df["prix_unitaire"] * df["quantite"]
+    return results.sum()
 
 
 def get_stats(df):
     min_price = get_min_price(df)
     max_price = get_max_price(df)
     avg_price = get_avg_price(df)
-    total_stock = get_total_stock(df)
+    total_stock = sum_multi_price_by_quantity(df)
 
     return {
         "min_price": min_price,
